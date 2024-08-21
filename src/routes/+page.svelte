ad3970143d6b0e78657702e267e5edf01ee9d980
Lukas Runge <login@lukasrunge.de>
2024-08-21 15:59:24 +0200

    🐛 fix according to https://github.com/sweetalert2/sweetalert2/issues/2752

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 	import { onMount } from 'svelte';
 
-	import Swal from 'sweetalert2';
+	import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 	onMount(() => {
 		Swal.fire('This thing works!');
